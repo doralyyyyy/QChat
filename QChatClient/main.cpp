7aacd4d7c99f1a5bcbdc7ad2b818bf21f31f08ac
doralyyyyy <117529262+doralyyyyy@users.noreply.github.com>
2025-05-09 00:16:03 +0800

    Update main.cpp

--- QChatClient/main.cpp
+++ QChatClient/main.cpp
@@ -9,14 +9,14 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("QChat");
     QCoreApplication::setApplicationName("QChatClient");
 
-    // LoginWindow l;
-    // l.show();
+    LoginWindow l;
+    l.show();
 
     // 注释掉上面两行，给下面四行取消注释，修改第一个传入参数，可直接进入main_window测试
-    Client *client = new Client("10.7.54.26", 11455);
-    MainWindow w(client);
-    client->mainWindow = &w;
-    w.show();
+    // Client *client = new Client("localhost", 11455);
+    // MainWindow w(client);
+    // client->mainWindow = &w;
+    // w.show();
 
     return a.exec();
 }
